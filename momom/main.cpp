008130cd4f6d411a1da27c03c2ed081daeb52853
Sven de Ridder <sven@svenr.org>
2013-09-23 17:16:51 +0200

    Initial Commit

---
+++ momom/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  momom
+//
+//  Created by Sven de Ridder on 9/23/13.
+//  Copyright (c) 2013 svenr. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
