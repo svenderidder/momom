81647964730c762ca6bf7dd5bb35438bc3c4f030
Sven de Ridder <sven@svenr.org>
2013-09-26 14:41:37 +0200

    Removed old streaming code.

--- momom/SavegameData.cpp
+++ momom/SavegameData.cpp
@@ -26,25 +26,4 @@ namespace momom {
     
     SavegameData::~SavegameData() {}
     
-    /*
-    std::istream& operator>>(std::istream& is, SavegameData& d) {
-        for(int w = 0; w < 5; ++w) {
-            for(int h = 0; h < 35; ++h) {
-                is >> d.herodata[w*35 + h];
-            }
-        }
-        
-        is >> d.unknown;
-        
-        is >> d.general;
-        
-        for(int i = 0; i < 5; ++i) {
-            is >> d.wizards[i];
-        }
-
-        return is;
-    }
-     
-    */
-
 }
