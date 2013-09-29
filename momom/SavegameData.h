679603d758cb65702fba6d1e280864329d068951
Sven de Ridder <sven@svenr.org>
2013-09-29 12:07:39 +0200

    Added unit fields.

--- momom/SavegameData.h
+++ momom/SavegameData.h
@@ -89,6 +89,31 @@ namespace momom {
     struct Enchantments: F<WizardRegion, uint8_t[24], 0x0482> {};
     struct PersonalSchool: F<WizardRegion, uint16_t, 0x04C4> {};
 
+    struct UnitRegion: Region<0xB734, 32, 1000> {};
+    struct UnitX: F<UnitRegion, uint8_t, 0x0000> {};
+    struct UnitY: F<UnitRegion, uint8_t, 0x0001> {};
+    struct UnitP: F<UnitRegion, uint8_t, 0x0002> {};
+    struct UnitOwner: F<UnitRegion, uint8_t, 0x0003> {};
+    struct UnitTotalMoves: F<UnitRegion, uint8_t, 0x0004> {};
+    struct UnitType: F<UnitRegion, uint8_t, 0x0005> {};
+    struct UnitHeroSlot: F<UnitRegion, uint8_t, 0x0006> {};
+    struct UnitMoved: F<UnitRegion, uint8_t, 0x0007> {};
+    struct UnitRemainingMoves: F<UnitRegion, uint8_t, 0x0008> {};
+    struct UnitDestinationX: F<UnitRegion, uint8_t, 0x0009> {};
+    struct UnitDestinationY: F<UnitRegion, uint8_t, 0x000A> {};
+    struct UnitStatus: F<UnitRegion, uint8_t, 0x000B> {};
+    struct UnitLevel: F<UnitRegion, uint8_t, 0x000C> {};
+    struct UnitExperience: F<UnitRegion, uint16_t, 0x000E> {};
+    struct UnitLifeDrain: F<UnitRegion, uint8_t, 0x0010> {};
+    struct UnitDamage: F<UnitRegion, uint8_t, 0x0011> {};
+    struct UnitGrouping: F<UnitRegion, uint8_t, 0x0012> {};
+    struct UnitScoutingRange: F<UnitRegion, uint8_t, 0x0016> {};
+    struct UnitWeaponMutation: F<UnitRegion, uint8_t, 0x0017> {};
+    struct UnitEnchantments: F<UnitRegion, uint32_t, 0x0018> {};
+    struct UnitRoadLeft: F<UnitRegion, uint8_t, 0x001C> {};
+    struct UnitRoadX: F<UnitRegion, uint8_t, 0x001D> {};
+    struct UnitRoadY: F<UnitRegion, uint8_t, 0x001E> {};
+    
     class SavegameData {
     public:
         SavegameData(const char* filename)
